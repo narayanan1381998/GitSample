60e0f3e34296802cb3d309e4366c734d050da8fd
narayanan1381998 <narayanan1381998@gmail.com>
2022-12-07 13:05:14 +0530

    Initial code

--- src/main/java/org/git/Class1.java
+++ src/main/java/org/git/Class1.java
@@ -1,5 +1,19 @@
 package org.git;
 
 public class Class1 {
+	private void method1() {
+		System.out.println("1");
+
+	}
+	private void method2() {
+		System.out.println("2");
+
+	}
+
+	private void method3() {
+		System.out.println("3");
+
+	}
+
 
 }
